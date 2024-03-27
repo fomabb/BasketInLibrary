50f2f9a89583fceae93e6863d0f41cca5545947d
fomabb <fomabb@yandex.by>
2024-03-27 19:53:38 +0300

    create configuration add indexes for table

---
+++ src/main/java/com/iase24/springjunit/config/DatabaseInitializer.java
@@ -0,0 +1,23 @@
+package com.iase24.springjunit.config;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import javax.sql.DataSource;
+
+@Configuration
+@RequiredArgsConstructor
+public class DatabaseInitializer {
+
+    private final DataSource dataSource;
+
+    @PostConstruct
+    public void init() {
+            ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+            populator.addScript(new ClassPathResource("init_database.sql"));
+            populator.execute(dataSource);
+    }
+}
