efa5829c2b52e11db8426e0476374a882782cda6
fomabb <fomabb@yandex.by>
2024-04-07 00:13:37 +0300

    fix

--- src/main/java/com/iase24/springjunit/service/imple/UserServiceImpl.java
+++ src/main/java/com/iase24/springjunit/service/imple/UserServiceImpl.java
@@ -27,7 +27,7 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
+@Transactional(readOnly = true)
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
