beeeeb110162f77454e3534bdda04a14f5ca47cb
fomabb <fomabb@yandex.by>
2024-04-16 13:30:54 +0300

    FlyWay migration

--- src/main/resources/init_database.sql
+++ src/main/resources/init_database.sql
@@ -4,15 +4,25 @@ CREATE INDEX IF NOT EXISTS idx_fts_articles ON book
 CREATE EXTENSION IF NOT EXISTS pg_trgm;
 
 CREATE INDEX IF NOT EXISTS book_trgm_idx ON book USING gin (title gin_trgm_ops);
-
-
--- insert into role(name)
--- values ('ROLE_ADMIN');
 --
--- insert into role(name)
--- values ('ROLE_USER');
 --
--- insert into users(basket_id, cart_id, email, password, username)
--- values (1, 1, 'admin@gmail.com', '$2a$12$fYUpZNlqvSLJ5ZBqfPJHsenEtP4/vE46gybdgaozDMN6bA43CgAI.', 'admin')
-
-
+-- -- insert into role(name)
+-- -- values ('ROLE_ADMIN');
+-- --
+-- -- insert into role(name)
+-- -- values ('ROLE_USER');
+-- --
+-- -- insert into users(basket_id, cart_id, email, password, username)
+-- -- values (1, 1, 'admin@gmail.com', '$2a$12$fYUpZNlqvSLJ5ZBqfPJHsenEtP4/vE46gybdgaozDMN6bA43CgAI.', 'admin')
+--
+-- -- create or replace function make_tsvector(title varchar, genre varchar, author varchar)
+-- --     returns tsvector as
+-- -- $$
+-- -- begin
+-- --     return (setweight(to_tsvector('english', title), 'A') ||
+-- --             setweight(to_tsvector('english', genre), 'B') ||
+-- --             setweight(to_tsvector('english', author), 'C'));
+-- -- end
+-- -- $$ language 'plpgsql' immutable;
+--
+--
