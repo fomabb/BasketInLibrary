d080964c112f5287ddfa3ad5acd147803180c0b6
Nikolay Kirilyuk <kirabb123@gmail.com>
2025-03-14 14:39:07 +0300

    fixing migration

--- src/main/resources/db/migration/V11__create_tables_faq_description.sql
+++ src/main/resources/db/migration/V11__create_tables_faq_description.sql
@@ -10,8 +10,8 @@ CREATE TABLE IF NOT EXISTS faq
 (
     id                   BIGSERIAL PRIMARY KEY,
     question             VARCHAR(255),
-    date_question_create TIMESTAMP WITHOUT TIME ZONE,
+    date_question_create TIMESTAMP(6),
     answer               VARCHAR(255),
-    date_answer_create   TIMESTAMP WITHOUT TIME ZONE,
+    date_answer_create   TIMESTAMP(6),
     description_id BIGINT REFERENCES description(id)
 );
--- src/main/resources/db/migration/V3__create_table_carts.sql
+++ src/main/resources/db/migration/V3__create_table_carts.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS carts
 (
     id         BIGSERIAL PRIMARY KEY,
-    dateCreate TIMESTAMP,
-    allPrice   DECIMAL
+    all_price   NUMERIC(38, 2),
+    date_create TIMESTAMP(6)
 );
