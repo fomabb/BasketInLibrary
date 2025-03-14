e9fcc99c8a6c1d6325460ab56e16c9f2f6f9c118
Nikolay Kirilyuk <kirabb123@gmail.com>
2025-03-14 12:27:17 +0300

    settings profile active

--- src/main/resources/db/migration/V7__create_relation_sheep_products_orders.sql
+++ src/main/resources/db/migration/V7__create_relation_sheep_products_orders.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS products_orders
 (
     id                       BIGSERIAL PRIMARY KEY,
     creation_time            TIMESTAMP,
-    delivery_report_date     VARCHAR(50),
+    delivery_report_date     TIMESTAMP(6) WITHOUT TIME ZONE,
     report_on_the_event_date TIMESTAMP,
     status_delivery_id       INTEGER,
     product_id               BIGINT REFERENCES products (id),
