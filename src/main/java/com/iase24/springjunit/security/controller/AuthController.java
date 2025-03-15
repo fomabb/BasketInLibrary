231fc82372cdde47b33fd95c7ab6202143c42a11
Nikolay Kirilyuk <kirabb123@gmail.com>
2025-03-15 20:45:26 +0300

    add swagger doc to auth controller

--- src/main/java/com/iase24/springjunit/security/controller/AuthController.java
+++ src/main/java/com/iase24/springjunit/security/controller/AuthController.java
@@ -3,6 +3,9 @@ package com.iase24.springjunit.security.controller;
 import com.iase24.springjunit.security.dto.JwtRequest;
 import com.iase24.springjunit.security.dto.RegistrationUserDTO;
 import com.iase24.springjunit.security.facade.AuthFacade;
+import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -16,21 +19,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@Tag(name = "Аутентификация и регистрация", description = "API для управлением безопасности")
 public class AuthController {
 
     private final AuthFacade authFacade;
 
-    @PostMapping
+    @Operation(
+            summary = "Вход в приложение.",
+            description = """
+                    ```В теле запроса необходимо прописать имя и пароль пользователя.```
+                    """
+    )
+    @PostMapping("/sign-in")
     public ResponseEntity<?> createAuthToken(@RequestBody JwtRequest authRequest) {
         return authFacade.createAuthToken(authRequest);
     }
 
-    @PostMapping("/registration")
+    @Operation(
+            summary = "Регистрация нового пользователя.",
+            description = """
+                    ```В теле запроса необходимо ввести все необходимые для регистрации данные о пользователе.```
+                    """
+    )
+    @PostMapping("/sign-up")
     public ResponseEntity<?> createNewUser(@RequestBody RegistrationUserDTO registrationUserDTO) {
         return authFacade.createNewUser(registrationUserDTO);
     }
 
     //TODO
+    @Hidden
     @PutMapping("/updateRole/{userId}")
     public ResponseEntity<?> updateUserRole(@PathVariable("userId") Long userId) {
         return authFacade.updateRoleUser(userId);
